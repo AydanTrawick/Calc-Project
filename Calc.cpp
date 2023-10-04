57903fe84397a7fbda220f0c304347406addbf31
AydanTrawick <146544795+AydanTrawick@users.noreply.github.com>
2023-10-04 15:23:23 -0400

    Update Calc.cpp

--- Calc.cpp
+++ Calc.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    // TODO: Declare necessary variables
+    // Declare necessary variables
     double num1,num2;
     char opp;
-    // TODO: Implement logic to perform the arithmetic operation based on user input
+    // Implement logic to perform the arithmetic operation based on user input
     cout << "Enter the first number: ";
     cin >> num1;
     cout << "Enter the second number: ";
     cin >> num2;
     cout << "Enter the operation (+, -, *, /): ";
     cin >> opp;
-    // TODO: Display the result
+    // Display the result
     if(opp=='+'){
         cout << "Result: " << num1 + num2 << endl;
     }else if(opp=='-'){
